f10b0ee9da75cc35032f01849fca3b0942c7e8b9
Łukasz Biały <lukasz@keios.eu>
2015-09-28 11:29:08 +0200

    initial commit

---
+++ Plugin.php
@@ -0,0 +1,26 @@
+<?php namespace Keios\DualFormWidget;
+
+use System\Classes\PluginBase;
+
+/**
+ * DualFormWidget Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'DualFormWidget',
+            'description' => 'No description provided yet...',
+            'author'      => 'Keios',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+}
---
+++ formwidgets/DualFormWidget.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Łukasz Biały
+ * URL: http://keios.eu
+ * Date: 9/28/15
+ * Time: 11:28 AM
+ */
+
+namespace Keios\DualFormWidget\FormWidgets;
+
+
+class DualFormWidget 
+{
+
+}
